c6975a97ce745559f479e10bccf4dbe2f2b1f678
Keonhee Park <dev.keonhee@gmail.com>
2023-06-02 17:50:46 +0900

    Update SettingEditor - Tag 기반 Viewer 작업 중

--- Assets/Editor/Scripts/SettingModuleEditor/SettingModuleEditor.cs
+++ Assets/Editor/Scripts/SettingModuleEditor/SettingModuleEditor.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.Linq;
 
 public partial class SettingModuleEditor : EditorWindow
 {
@@ -9,6 +10,7 @@ public partial class SettingModuleEditor : EditorWindow
     static private Vector2 windowSize = new Vector2(1080, 1080);
 
     private Vector2 scrollPosition_SettingViewer = Vector2.zero;
+    private List<Vector2> scrollPositionList_SettingTagViewer = new List<Vector2>();
 
     private SettingModuleManager settingModuleManager;
     private SettingModuleManager SettingModule
@@ -21,10 +23,17 @@ public partial class SettingModuleEditor : EditorWindow
         }
     }
 
+    private List<string> settingTags = new List<string>();
+
     [MenuItem("UnityModularization/Setting Module Editor")]
     static void OpenWindow()
     {
         EditorWindow.GetWindowWithRect<SettingModuleEditor>(new Rect(windowPosition, windowSize), true, "Setting Module Editor");
+    }
+
+    private void OnEnable()
+    {
+
     }
 
     private void Update()
@@ -41,7 +50,15 @@ public partial class SettingModuleEditor : EditorWindow
         {
             scrollPosition_SettingViewer = EditorGUILayout.BeginScrollView(scrollPosition_SettingViewer, style_Scroll_SettingViewer);
             {
+                var settingTags = settingModuleManager.ItemsInRuntime.Select(item => item.Tag).Distinct().OrderBy(tag => tag);
 
+                foreach (var tag in settingTags)
+                {
+                    GUILayout.Label(tag, style_SettingTag_Header);
+                    {
+
+                    }
+                }
             }
         }
         #endregion
