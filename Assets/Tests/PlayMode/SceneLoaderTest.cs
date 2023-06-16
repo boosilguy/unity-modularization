0b253cd0a7bc2f94dbc2f4b6fb94a05a7e149696
Keonhee Park <dev.keonhee@gmail.com>
2023-06-16 17:06:26 +0900

    Update ModuleManagerTest - Init한 Manager 내 모듈이 생존해 있는지 확인

--- Assets/Tests/PlayMode/SceneLoaderTest.cs
+++ Assets/Tests/PlayMode/SceneLoaderTest.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using setting;
 using UnityEngine;
@@ -11,28 +12,37 @@ public class SceneLoaderTest
     private GameObject moduleManagerGameObject;
     private ModuleManager moduleManager;
     public static string[] sceneName = { "SceneA" };
-    public static string[] moduleFoundation = { "SettingManagerModule" };
+    public static string[] moduleFoundation = { "SettingModuleManager" };
 
     [UnitySetUp]
     public IEnumerator SetUp()
     {
-        yield return null;
         moduleManagerGameObject = new GameObject("ModuleManager");
         moduleManager = moduleManagerGameObject.AddComponent<ModuleManager>();
+        ModuleInitUtil.InitModules(moduleManager);
+        yield return null;
     }
 
     [UnityTest]
-    public IEnumerator SceneChange([ValueSource(nameof(sceneName))] string sceneName)
+    public IEnumerator SceneChange([ValueSource(nameof(sceneName))] string scene, [ValueSource(nameof(moduleFoundation))] string module)
     {
-        //ModuleInitUtil.InitModules(moduleManager);
-
         Assert.IsTrue(moduleManagerGameObject == GameObject.FindObjectOfType<ModuleManager>().gameObject);
         Debug.Log($"InitializeScene => {moduleManagerGameObject == GameObject.FindObjectOfType<ModuleManager>().gameObject}");
 
         yield return null;
 
-        SceneManager.LoadScene(sceneName);
+        SceneManager.LoadScene(scene);
         Assert.IsTrue(moduleManagerGameObject == GameObject.FindObjectOfType<ModuleManager>().gameObject);
-        Debug.Log($"{sceneName} => {moduleManagerGameObject == GameObject.FindObjectOfType<ModuleManager>().gameObject}");
+        Debug.Log($"{scene} => {moduleManagerGameObject == GameObject.FindObjectOfType<ModuleManager>().gameObject}");
+
+        Debug.Log($"Module count => {ModuleManager.Instance.modules.Count()}");
+        foreach(var item in ModuleManager.Instance.modules)
+        {
+            Debug.Log($"Module name is {item.GetType().Name}");
+            Debug.Log($"Module is null {item == null}");
+        }
+
+        Assert.IsNotNull(ModuleManager.Instance.GetModule(module));
+        Debug.Log($"Module name => {ModuleManager.Instance.GetModule(module).GetType().Name}");
     }
 }
